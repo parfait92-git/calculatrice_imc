f3d708fa2544dbef5f95c53528bfde69154d1320
parfait92-git <parfaitaristantnzo@gmail.com>
2021-02-24 11:48:29 +0100

    cameroonnitech

--- utils/fetchData.js
+++ utils/fetchData.js
@@ -1,4 +1,4 @@
-const baseURL = "https://calculatrice-iimc.vercel.app";
+const baseURL = "https://cameroonnitech.vercel.app";
 
 export const postData = async (url, payload, token) => {
     const res = await fetch(`${baseURL}/api/${url}`, {
